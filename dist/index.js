26b6f22b231faedc19d2ba4509ad8e980ebe4bae
ChadM <chadmccaulley95@gmail.com>
2022-02-17 13:22:26 -0700

    [fix] Updates build files

--- dist/index.js
+++ dist/index.js
@@ -723,6 +723,7 @@
     props: VirtualProps,
     data: function data() {
       return {
+        prevOffset: 0,
         range: null
       };
     },
@@ -821,7 +822,14 @@
           return document.documentElement[this.directionKey] || document.body[this.directionKey];
         }
 
-        if (this.scrollElement) return this.scrollElement[this.directionKey];
+        if (this.scrollElement) {
+          var scrollLoc = this.scrollElement[this.directionKey];
+          var offset = scrollLoc - this.getVirtualTopOffset();
+          if (offset < this.prevOffset) offset += this.bottomOffset;
+          this.prevOffset = scrollLoc;
+          return offset > 0 ? offset : 0;
+        }
+
         var root = this.$refs.root;
         return root ? Math.ceil(root[this.directionKey]) : 0;
       },
@@ -832,7 +840,7 @@
         if (this.pageMode) {
           return document.documentElement[key] || document.body[key];
         } else if (this.scrollElement) {
-          return this.scrollElement[key];
+          return this.scrollElement[key] - this.getVirtualTopOffset();
         } else {
           var root = this.$refs.root;
           return root ? Math.ceil(root[key]) : 0;
@@ -850,6 +858,19 @@
           var root = this.$refs.root;
           return root ? Math.ceil(root[key]) : 0;
         }
+      },
+      // return the offset from the top of the scrollbar to the start of the virtual list
+      getVirtualTopOffset: function getVirtualTopOffset() {
+        var elementTopOffset = this.$el.getBoundingClientRect().top;
+        var scrollerTopOffset = this.scrollElement.getBoundingClientRect().top;
+        var offset = elementTopOffset + this.scrollElement[this.directionKey];
+
+        if (scrollerTopOffset > 0) {
+          if (elementTopOffset < 0) return 0;
+          offset = elementTopOffset - scrollerTopOffset;
+        }
+
+        return offset > 0 ? offset : 0;
       },
       // set current scroll position to a expectant offset
       scrollToOffset: function scrollToOffset(offset) {
